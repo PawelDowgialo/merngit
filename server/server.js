e6a88fb892bf748b470ebde60d31dc5ac0c8b513
pablo <paweldowgialo@gmail.com>
2024-03-14 15:32:17 +0100

    add db

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express()
+const PORT = 8000
+
+app.use(express.json())
+
+const myDataBase = "myDB"
+const url = `mongodb://localhost:27017/${myDataBase}`
+mongoose.connect(url)
+    .then(()=>console.log("Connected"))
+    .catch((err)=>console.log("Connection Error", err.message))
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number
+})
+
+const user = mongoose.model("User", userSchema)
+
+app.get("/api/users", async(req, res)=>{
+    try{
+       const users = await UserActivation.find({})
+       res.json(users)
+    }
+    catch(err){
+        res.status(500).json({message: err.message})
+    }
+})
+
+app.listen(PORT, ()=>console.log("Server wydupcyło na porcie", PORT))
+
+process.on("SIGINT", ()=>{
+    console.log("Closing MongoDB")
+    mongoose.disconnect()
+        .then(()=>console.log("MongoDB connection closed"))
+        .finally(()=>process.exit())
+})
